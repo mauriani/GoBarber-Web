387742999fc68cfa762d771b785fd6dcb6040013
mauriani <maurianim@gmail.com>
2021-03-24 16:55:10 -0300

    first commit

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+
+import logo from '../../assets/logo.svg';
+import { Container, Content, Background } from './styles';
+
+const SignIn: React.FC = () => {
+  return (
+    <Container>
+      <Content>
+        <img src={logo} alt="GoBarber" />
+
+        <form>
+          <h1>Faça seu logon</h1>
+
+          <Input name="email" placeholder="E -mail" icon={FiMail} />
+          <Input
+            name="password"
+            type="password"
+            placeholder="Senha"
+            icon={FiLock}
+          />
+
+          <Button type="submit">Entrar</Button>
+
+          <a href="forgot">Esqueci minha senha</a>
+        </form>
+
+        <a href="conta">
+          <FiLogIn />
+          Criar Conta
+        </a>
+      </Content>
+      <Background />
+    </Container>
+  );
+};
+
+export default SignIn;
---
+++ src/pages/SingUp/index.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
+import { Form } from '@unform/web';
+
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+
+import logo from '../../assets/logo.svg';
+import { Container, Content, Background } from './styles';
+
+const SignUp: React.FC = () => {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
+  return (
+    <Container>
+      <Background />
+      <Content>
+        <img src={logo} alt="GoBarber" />
+
+        <Form onSubmit={handleSubmit}>
+          <h1>Faça seu Cadastro</h1>
+
+          <Input name="name" placeholder="Nome" icon={FiUser} />
+          <Input name="email" placeholder="E -mail" icon={FiMail} />
+          <Input
+            name="password"
+            type="password"
+            placeholder="Senha"
+            icon={FiLock}
+          />
+
+          <Button type="submit">Cadastrar</Button>
+        </Form>
+
+        <a href="conta">
+          <FiArrowLeft />
+          Voltar para logon
+        </a>
+      </Content>
+    </Container>
+  );
+};
+
+export default SignUp;
