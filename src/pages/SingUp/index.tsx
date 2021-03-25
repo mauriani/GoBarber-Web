61e4949f0abed3de8ffe20363c13e975ccecf364
mauriani <maurianim@gmail.com>
2021-03-25 11:58:34 -0300

    subindo alterações

--- src/pages/SingUp/index.tsx
+++ src/pages/SingUp/index.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
+import { FormHandles } from '@unform/core';
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
 import { Form } from '@unform/web';
+import * as Yup from 'yup';
+import getValidationErros from '../../utils/getValidationErrors';
 
 import Button from '../../components/Button';
 import Input from '../../components/Input';
@@ -8,18 +11,41 @@ import Input from '../../components/Input';
 import logo from '../../assets/logo.svg';
 import { Container, Content, Background } from './styles';
 
+interface SignUpForm {
+  name: string;
+  email: string;
+  password: string;
+}
+
 const SignUp: React.FC = () => {
-  // eslint-disable-next-line @typescript-eslint/ban-types
-  function handleSubmit(data: object): void {
-    console.log(data);
-  }
+  const formRef = useRef<FormHandles>(null);
+
+  const handleSubmit = useCallback(async (data: SignUpForm) => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string()
+          .required('Email obrigatório')
+          .email('Digite um e-mail válido'),
+        password: Yup.string().min(
+          6,
+          'Sua senha deve ter no minimo seis caracteres',
+        ),
+      });
+
+      await schema.validate(data, { abortEarly: false });
+    } catch (err) {
+      const errors = getValidationErros(err);
+      formRef.current?.setErrors(errors);
+    }
+  }, []);
   return (
     <Container>
       <Background />
       <Content>
         <img src={logo} alt="GoBarber" />
 
-        <Form onSubmit={handleSubmit}>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Faça seu Cadastro</h1>
 
           <Input name="name" placeholder="Nome" icon={FiUser} />
