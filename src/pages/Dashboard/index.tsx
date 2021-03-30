16b2c8593b9aae62bd67733e752703a7e0a03070
mauriani <maurianim@gmail.com>
2021-03-30 12:38:23 -0300

    autentificação de rotas

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <div />;
+  return <h1>Dashboard</h1>;
 };
 
 export default Dashboard;
